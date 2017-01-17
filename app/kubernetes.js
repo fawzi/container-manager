c77a3717db3e4174cc2768a6656c4b0768492ab5
Ankit Kariryaa <kariryaa@fhi-berlin.mpg.de>
2017-01-17 19:28:19 +0100

    Make kubernetes ca files etc optional

--- app/kubernetes.js
+++ app/kubernetes.js
@@ -2,17 +2,19 @@ const K8Api = require('kubernetes-client'),
       fs = require('fs');
 
 module.exports = function (config) {
-
-  const k8 = new K8Api.Core({
+  var k8options = {
     url: config.k8Api.url,
     version: 'v1',  // Defaults to 'v1'
-    namespace: 'default', // Defaults to 'default',
-    if(config.k8Api.ca){
-      ca: fs.readFileSync(config.k8Api.ca),
-      cert: fs.readFileSync(config.k8Api.cert),
-      key: fs.readFileSync(config.k8Api.key)
-    }
-  });
+    namespace: 'default' // Defaults to 'default',
+  }
+
+  if(config.k8Api.ca){
+    k8options.ca = fs.readFileSync(config.k8Api.ca);
+    k8options.cert = fs.readFileSync(config.k8Api.cert);
+    k8options.key = fs.readFileSync(config.k8Api.key);
+  }
+
+  const k8 = new K8Api.Core(k8options);
 
   function print(err, result) {
     console.log(JSON.stringify(err || result, null, 2));
