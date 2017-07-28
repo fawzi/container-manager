2a072496af06e370e0437e3abe6853fd93a223c4
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2017-07-28 16:19:35 +0200

    removing duplicate login routes

--- config/routes.js
+++ config/routes.js
@@ -34,34 +34,6 @@ module.exports = function (app, redirect, config, proxyServer, proxyRouter, k8, 
     return selfName
   }
 
-  app.get(config.app.localOverride,ensureLoggedIn('/login'),function (req, res) {
-    var userID = (req.user.id === undefined) ?  'unknownSess1' : req.user.id;
-    console.log('Get localoverride:' + userID);
-    proxyRouter.client.hget(userID, config.app.localOverride,function(err,data){
-      if(data){
-        var target = JSON.parse(data)
-        res.send(`<h1>HOST: ${target.host}</h1> <h1>PORT: ${target.port}</h1>`)
-      }
-      else
-        res.send(`<h1>Not set yet!</h1>`)
-    });
-
-  });
-
-  app.post(config.app.localOverride,function (req, res) {
-    if(req.query.host && req.query.port){
-      var target = {host: req.query.host, port: req.query.port};
-      var writeTarget = JSON.stringify(target);
-
-      proxyRouter.client.hset(req.query.user, config.app.localOverride, writeTarget,function(err,data){
-        proxyRouter.client.expireat(req.query.user, 60*10);
-        res.send(`<h1>Set! ${req.query.user} ${writeTarget}</h1>`);
-      });
-    }
-    else {
-      console.log(`Not executing! ${JSON.stringify(req.query)}`)
-    }
-  });
 
   app.get('/nmdalive', function(req, res){
     res.send("<div>Hello2!!</div>");
