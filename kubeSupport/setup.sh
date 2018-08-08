fd01be11ad00b16166bcb0e70ceb5c35d0511e0f
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2018-08-08 10:56:37 +0200

    create pass if not given, removing containerPerSession, adding imageReStr

--- kubeSupport/setup.sh
+++ kubeSupport/setup.sh
@@ -1,8 +1,16 @@
 target_hostname=${target_hostname:-$HOSTNAME}
 cat <<EOF
+# once (create secrets)
+if [ ! -e redis-session-db-pwd.txt ]; then
+  cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 > redis-session-db-pwd.txt
+fi
 kubectl create secret generic redis-session-db-pwd --from-file=./redis-session-db-pwd.txt
-helm install --name redis-session-db -f redis-session-db-values.yaml stable/redis
+if [ ! -e redis-user-db-pwd.txt ]; then
+  cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 > redis-user-db-pwd.txt
+fi
 kubectl create secret generic redis-user-db-pwd --from-file=./redis-user-db-pwd.txt
+
+helm install --name redis-session-db -f redis-session-db-values.yaml stable/redis
 kubectl apply -f redis-user-db-volume-$target_hostname.yaml
 helm install --name redis-user-db -f redis-user-db-values.yaml stable/redis
 EOF
