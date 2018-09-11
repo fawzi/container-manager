9dce834a4cfd2b8dccec41a4a1e9ad97e0275974
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2018-09-11 14:52:27 +0200

    ProxyRouter cleanup
    * fixed pod creation error
    * better logging and info when waiting/failing

--- app/ProxyRouter.js
+++ app/ProxyRouter.js
@@ -14,7 +14,7 @@ function guaranteeDir(path, next) {
         if(err) throw err;
         fs.chown(path, 1000, 1000, (err) => {
           if (err)
-            console.log('Dir '+ path + ' created, error in chown: ' + JSON.stringify(err));
+            console.log('Dir '+ path + ' created, error in chown: ' + stringify(err));
           else
             console.log('Dir correctly created:' + path);
           next();
@@ -42,18 +42,17 @@ function getOrCreatePod(podName, repl, shouldCreate, next) {
       if (shouldCreate) {
         components.templateForImage(repl, function(err, template, repl) {
           if(err) {
-            console.log(`#ERROR# Cannot start pod ${podName}, error in template generation: ${JSON.stringify(err)}`);
+            console.log(`#ERROR# Cannot start pod ${podName}, error in template generation: ${stringify(err)}`);
             next(err, null)
           } else {
-            //fs.writeFile("pod.yaml", template)
-            //console.log("wrote evaulated template to pod.yaml")
             guaranteeUserDir(repl.user, function (){
-              k8.ns(config.k8component.namespace).service.post({ body: yaml.safeLoad(template, 'utf8')}, function(err, res2){
+              const templateValue = yaml.safeLoad(template, 'utf8')
+              k8.ns(config.k8component.namespace).pod.post({ body: templateValue}, function(err, res2){
                 if(err) {
-                  console.log(`#ERROR# Cannot start pod ${podName}, error: ${JSON.stringify(err)}`);
+                  console.log(`#ERROR# Cannot start pod ${podName}, error: ${stringify(err)}, \n====\ntemplate was ${template}\n====`);
                   next(err, null)
                 } else {
-                  console.log(`Created pod ${podName}: ${JSON.stringify(res2)}`)
+                  console.log(`Created pod ${podName}: ${stringify(res2)}`)
                   next(null, res2)
                 }
               })
@@ -61,11 +60,11 @@ function getOrCreatePod(podName, repl, shouldCreate, next) {
           }
         });
       } else {
-        console.log(`#ERROR# requested pod ${podName} which does not exist and should not be created, error: ${JSON.stringify(err)}`);
+        console.log(`#ERROR# requested pod ${podName} which does not exist and should not be created, error: ${stringify(err)}`);
         next(err, null)
       }          
     } else {
-      console.log(`looked up ${podName}: ${JSON.stringify(result)}`)
+      console.log(`looked up ${podName}: ${stringify(result)}`)
       next(null, result)
     }
   });
@@ -87,14 +86,46 @@ function resolvePod(repl, next) {
         if (err) {
           next(err, null)
         } else {
-          const portInfo = pod.spec.containers[0].ports[0]
-          const res = {
-            host: portInfo.hostIP,
-            port: portInfo.containerPort
+          const portNr = pod.spec.containers[0].ports[0].containerPort
+          const podIp = pod.status.podIP
+          if (podIp) {
+            var ready = false
+            const conds = pod.status.conditions
+            if (pod.status && conds) {
+              for (icond in conds) {
+                let cond = conds[icond]
+                if (cond.type === 'Ready' && cond.status === 'True')
+                  ready = true
+              }
+            }
+            if (ready) {
+              const res = {
+                host: podIp,
+                port: portNr
+              }
+              console.log(`got ${stringify(res)} out of pod ${stringify(pod)}`)
+              resolveCache.set(podName, res)
+              next(null, res)
+            } else {
+              const err = {
+                error: "not ready",
+                msg: "pod not yet ready",
+                status: pod.status,
+                host: podIp,
+                port: portNr
+              }
+              next(err, null)
+            }
+          } else {
+            const err = {
+              error: "no ip",
+              msg: "ip not yet available",
+              status: pod.status,
+              host: podIp,
+              port: portNr
+            }
+            next(err, null)
           }
-          console.log(`got ${JSON.stringify(res)} out of pod ${JSON.stringify(pod)}`)
-          resolveCache.set(podName, res)
-          next(null, res)
         }
       })
     } else {
@@ -112,16 +143,26 @@ function resolvePod(repl, next) {
 // The decision could be made using the state machine instead of the
 ProxyRouter.prototype.lookup = function(req, res, userID, isWebsocket, path, next) {
   console.log("Looking up the path! " + req.path)
+  var start = Date.now()
   components.cachedReplacements(req, function(err, repl) {
+    console.log(`replacements available after ${(Date.now()-start)/1000.0}`)
     if (err) {
-      console.log(`ERROR no replacements: lookup without visiting the entry point ${config.k8component.entryPoint.path} (${JSON.stringify(err)})`)
+      console.log(`ERROR no replacements: lookup without visiting the entry point ${config.k8component.entryPoint.path} (${stringify(err)})`)
     } else {
-      const podName = components.podNameForRepl(repl)
-      resolvePod(podName, function (err, target) {
+      resolvePod(repl, function (err, target) {
+        //console.log(`target available after ${(Date.now()-start)/1000.0}`)
         if (err) {
-          console.log(`ERROR ${JSON.stringify(err)}`)
+          console.log(`ERROR ${stringify(err)}`)
+          if (err.error === 'no ip' && err.status && err.status.phase === 'Pending' ||
+              err.error === 'not ready') {
+            console.log(`pod ${repl.podName} ${err.error} ${stringify(err)}`)
+            res.send(reloadMsg)
+          } else {
+            const errorMsg = `<html><head><title>Error starting Container!</title><meta http-equiv="refresh"<body><h3>Error ${err.error} while trying to start a container for you!</h3><p>${err.msg}</p><pre>${stringify(err, { spaces: 2 } )}</pre></body></html>`;
+            res.send(500, errorMsg)
+          }
         } else {
-          console.log(`Resolved to ${stringify(target)}`)
+          // console.log(`Resolved to ${stringify(target)}`)
           next(target);
         }
       })
